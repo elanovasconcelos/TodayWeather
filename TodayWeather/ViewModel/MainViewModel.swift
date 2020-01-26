f28b857a75a9e1091fe4e8c2d8b714dc7d661041
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-01-26 09:26:08 -0300

    - [MainViewModel] adding ServerModel

--- TodayWeather/ViewModel/MainViewModel.swift
+++ TodayWeather/ViewModel/MainViewModel.swift
@@ -8,8 +8,77 @@
 
 import UIKit
 
+protocol MainViewModelDelegate: class {
+    func mainViewModelChangedData(_ model: MainViewModel)
+}
+
 final class MainViewModel: NSObject {
 
+    private let server: ServerModel
+    private var forecast: Forecast? { didSet { delegate?.mainViewModelChangedData(self) }}
+    
+    weak var delegate: MainViewModelDelegate?
+    
+    init(server: ServerModel = ServerModel.shared) {
+        self.server = server
+    }
+    
+    func update() { 
+        server.forecast { [weak self] (result) in
+            switch result {
+            case .failure(let error):
+                print("error: \(error)")
+            case .success(let forecast):
+                self?.forecast = forecast
+            }
+        }
+    }
+    
+    func numberOfSections() -> Int {
+        return 1
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        
+        if forecast == nil {
+            return 0
+        }
+        
+        return WeatherDetail.values().count
+    }
+    
+    func detail(for indexPath: IndexPath) -> (title: String, information: String) {
+        
+        let weatherDetail = WeatherDetail.forIndex(indexPath.row)
+        let currentInformation = information(for: weatherDetail)
+        
+        return (title: weatherDetail.rawValue, information: currentInformation)
+    }
+    
+    private func information(for detail: WeatherDetail) -> String {
+        return doubleFormat(doubleInformation(for: detail))
+    }
+    
+    private func doubleInformation(for detail: WeatherDetail) -> Double? {
+        switch detail {
+        case .apparentTemperature: return forecast?.currently.apparentTemperature
+        case .temperature: return forecast?.currently.temperature
+        case .humidity: return forecast?.currently.humidity
+        case .windSpeed: return forecast?.currently.windSpeed
+        case .uvIndex: return forecast?.currently.uvIndex
+        }
+    }
+    
+    private func doubleFormat(_ value: Double?) -> String {
+        
+        guard let value = value else { return ""}
+        
+        return "\(value)"
+    }
+}
+
+//MARK: -
+extension MainViewModel {
     enum WeatherDetail: String {
         case temperature = "Temperature"
         case apparentTemperature = "Apparent temperature"
@@ -30,49 +99,5 @@ final class MainViewModel: NSObject {
         static func forIndex(_ index: Int) -> WeatherDetail {
             return values()[index]
         }
-    }
-    
-    private let forecast: Forecast
-    
-    
-    init(forecast: Forecast = Forecast.debugValue()) {
-        self.forecast = forecast
-    }
-    
-    func numberOfSections() -> Int {
-        return 1
-    }
-    
-    func numberOfRowsInSection(_ section: Int) -> Int {
-        return WeatherDetail.values().count
-    }
-    
-    func detail(for indexPath: IndexPath) -> (title: String, information: String) {
-        
-        let weatherDetail = WeatherDetail.forIndex(indexPath.row)
-        let currentInformation = information(for: weatherDetail)
-        
-        return (title: weatherDetail.rawValue, information: currentInformation)
-    }
-    
-    private func information(for detail: WeatherDetail) -> String {
-        return doubleFormat(doubleInformation(for: detail))
-    }
-    
-    private func doubleInformation(for detail: WeatherDetail) -> Double? {
-        switch detail {
-        case .apparentTemperature: return forecast.currently.apparentTemperature
-        case .temperature: return forecast.currently.temperature
-        case .humidity: return forecast.currently.humidity
-        case .windSpeed: return forecast.currently.windSpeed
-        case .uvIndex: return forecast.currently.uvIndex
-        }
-    }
-    
-    private func doubleFormat(_ value: Double?) -> String {
-        
-        guard let value = value else { return ""}
-        
-        return "\(value)"
     }
 }
